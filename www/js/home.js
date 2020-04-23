e52ec9efb493776beac9b3dbe47e3bcb482891ed
Court191 <courtney.cox1@outlook.com>
2020-04-23 22:25:03 +0100

    Added social media links with js functionality

--- www/js/home.js
+++ www/js/home.js
@@ -2,12 +2,14 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "100%";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("social").style.width = "100%";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("social").style.width = "0";
 }
 function openNotes() {
   document.getElementById("note").style.width = "100%";

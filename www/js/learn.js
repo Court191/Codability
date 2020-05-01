2bb25498ad24e5b879180e72f8a95300e4899216
Court191 <courtney.cox1@outlook.com>
2020-05-02 00:15:25 +0100

    Style changes

--- www/js/learn.js
+++ www/js/learn.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
         type: 'GET',
         url: 'https://ll16clc.leedsnewmedia.net/Codability/www/php/learn.php',
         success: function(result) {
-            //var arr = JSON.parse(result);
+            var arr = JSON.parse(result);
             
                 console.log(result); 
             
-                /*for(var i = 0; i < arr.length; i++) {
+                for(var i = 0; i < arr.length; i++) {
                 var obj = arr[i];   
                 
                 var output = document.getElementById("resources");  
                 
                 //output.innerHTML += '<div class="comment-container"><div class="title">'+obj.title+'</div><div class="description">'+obj.description+'</div></div>';
                     
-                output.innerHTML += '<div class="comment-container"><div class="title">'+obj.title+'</div><div class="description">'+obj.description+'</div><div class="learnimage">'+obj.learnimage+'</div></div>'; 
+                output.innerHTML += '<div class="comment-container"><div class="title">'+obj.title+'</div><div class="description">'+obj.description+'</div></div>'; 
                 
-            } */
+            }
         }
     });
 });
